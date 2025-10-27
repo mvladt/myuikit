5048484cd8e3cfe62e21df0e1ccaf6011ded7b03
Влад Мелехин <vmel@webizi.ru>
2025-10-27 16:04:07 +0300

    Заготовка 'MyButton'

---
+++ src/components/MyButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="input flex-container" v-bind="$attrs">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.input {
+  font-family: inherit;
+  font-size: 0.9rem;
+  padding: 0.25rem 0.5rem;
+  /* border-radius: 5px; */
+}
+
+/* .input {
+  color: var(--color-text);
+  background-color: var(--color-background-soft);
+  border: 1px solid var(--color-border);
+}
+
+button:hover {
+  background-color: var(--color-background-mute);
+}
+button:active {
+  background-color: var(--color-background-soft);
+} */
+
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+}
+</style>
---
+++ src/experiments/ButtonExp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MyButton from "../components/MyButton.vue";
+import ExpContainer from "../ExpContainer.vue";
+</script>
+
+<template>
+  <ExpContainer>
+    <MyButton>Hello</MyButton>
+  </ExpContainer>
+</template>
+
+<style scoped></style>
