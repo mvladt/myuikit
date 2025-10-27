f38a3dee49cec9e4a9c444a90af799d7c4b47363
Влад Мелехин <vmel@webizi.ru>
2025-10-27 16:03:27 +0300

    Добавил 'ExpContainer'

---
+++ src/ExpContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="exp-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.exp-container {
+  min-height: calc(100vh - 4rem);
+  width: min(600px, 90vw);
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+}
+</style>
